eb73e5b5e10470700e544e02d88d307df2834235
LuisAngel <luisangel.salvat@gmail.com>
2021-02-04 20:53:41 -0500

    Tegunda parte (040221).

--- app/src/main/java/com/qbo/appcontrolesbasicoskea3/MainActivity.kt
+++ app/src/main/java/com/qbo/appcontrolesbasicoskea3/MainActivity.kt
@@ -2,10 +2,47 @@ package com.qbo.appcontrolesbasicoskea3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_lista.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val listapreferencias = ArrayList<String>()
+    private val listapersonas = ArrayList<String>()
+    private var estadocivil = ""
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    fun validarGenero(): Boolean{
+        var respuesta = true
+        if(rggenero.checkedRadioButtonId == -1){
+            respuesta = false
+        }
+        return respuesta
+    }
+
+    fun validarNombreApellido(): Boolean{
+        var respuesta = true
+        if(etnombre.text.toString().trim().isEmpty()){
+            etnombre.isFocusableInTouchMode = true
+            etnombre.requestFocus()
+            respuesta = false
+        }else if(etapellido.text.toString().trim().isEmpty()){
+            etapellido.isFocusableInTouchMode = true
+            etapellido.requestFocus()
+            respuesta = false
+        }
+        return respuesta
+    }
+
+    fun enviarMensaje(vista: View, mensaje: String){
+        Snackbar.make(vista, mensaje, Snackbar.LENGTH_LONG).show()
     }
 }
