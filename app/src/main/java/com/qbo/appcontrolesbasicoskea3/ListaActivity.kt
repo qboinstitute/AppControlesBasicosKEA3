e243c12c5578310f2ba83b5fbb1af65d812880de
LuisAngel <luisangel.salvat@gmail.com>
2021-02-04 20:04:19 -0500

    Tegunda parte (040221).

---
+++ app/src/main/java/com/qbo/appcontrolesbasicoskea3/ListaActivity.kt
@@ -0,0 +1,11 @@
+package com.qbo.appcontrolesbasicoskea3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista)
+    }
+}
